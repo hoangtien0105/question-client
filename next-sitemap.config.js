792ca8022548ef6fd9b85c82ba79276b4d633765
nhattien015 <tienit0105@gmail.com>
2022-07-14 13:40:53 +0700

    fix: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+
+const BASE_URL = 'https://solutionschecker.com';
+const config = {
+    siteUrl: BASE_URL,
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+    sitemapSize: 7000,
+
+}
+  
+module.exports =  config
